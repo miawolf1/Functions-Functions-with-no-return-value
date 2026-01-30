6c29b7d723bbe7fdddd589c59c6842db07ec7950
Wolf17 <miaspasojevic3@gmail.com>
2026-01-30 20:42:05 +0100

    Add files via upload

---
+++ no return value.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+void dosomething() //declaration of a function //void means that the function does not have a return value, no number values.
+{
+    cout << "hello" << endl; 
+}
+int main()
+{
+    dosomething();  //calling the function
+    dosomething();
+
+}
+/*think of a function as a person with a job, it this case there 
+  name is "dosomething" and they were told every time there name 
+  was called to say "hello". */
+
+ /*functions are usefull cus instad of writing lets say a long sentence 
+ or a command over and over again we just call that function when necessary, thing of it
+ like using abbriviations in english class.  */
